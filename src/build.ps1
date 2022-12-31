bddb315167ee4afdcf2e7ea5e95acade7354d232
Neil Enns <neile@live.com>
2022-12-30 16:39:26 -0800

    Add release building via GitHub workflow (#35)
    Co-authored-by: Neil Enns <{ID}+{username}@users.noreply.github.com>

--- src/build.ps1
+++ src/build.ps1
@@ -5,12 +5,12 @@ Push-Location (join-path $PSScriptRoot mod)
 Pop-Location
 
 Push-Location (join-path $PSScriptRoot app)
-npm install
+npm ci
 npm run build:client
 npm run build:mcdu-server
 Pop-Location
-$source="..\build\MCDU SERVER\cj4-mcdu-server.exe"
-$target="..\build\MCDU SERVER\cj4-mcdu-server-$version.exe"
+$source = "..\build\MCDU SERVER\cj4-mcdu-server.exe"
+$target = "..\build\MCDU SERVER\cj4-mcdu-server-$version.exe"
 Move-Item $source $target -Force
 
 Push-Location $PSScriptRoot
@@ -18,7 +18,10 @@ copy-item ..\readme.md ..\build -force
 copy-item ..\version ..\build -force
 $filename = "..\release\cj4-mcdu-$version.zip"
 if (-not (test-path ..\release)) {
-    mkdir ..\release
+  mkdir ..\release
 }
 Compress-Archive ..\build\* $filename -force
 pop-location
+
+# Ensures the GitHub workflow knows the script succeeded
+exit 0
